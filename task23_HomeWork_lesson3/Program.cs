4d9453e497f7f09f9382fc634f17a88e1fad941d
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-09-28 20:03:14 +0400

    task23

---
+++ task23_HomeWork_lesson3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void showSquareNumber(int number)
+{
+int count = 1;
+int result = new int();
+
+while(count <= number)
+{
+    result = (int)Math.Pow((number - (number - count)), 3);
+    count = count + 1;
+    Console.Write(result);
+    if(count < number + 1)
+        {
+           Console.Write(", "); 
+        }
+}
+}
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Для числа {userNumber} кубы от 1 до {userNumber} равны: ");
+showSquareNumber(userNumber);
+Console.WriteLine();
