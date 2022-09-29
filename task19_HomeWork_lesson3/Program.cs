274f428eaad9865341910ad8b672aa2a6b9073ce
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-09-29 15:43:02 +0400

    task19

--- task19_HomeWork_lesson3/Program.cs
+++ task19_HomeWork_lesson3/Program.cs
@@ -3,6 +3,30 @@
 14212 -> нет
 12821 -> да
 23432 -> да */
+          
+int invertedNumber (int number)
+{
+    int result = 0;
+ 
+    while (number > 0) 
+    {
+        result = result * 10;
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int checkPalindrome = invertedNumber(userNumber);
+
+if(userNumber == checkPalindrome)
+{
+    Console.WriteLine("Введенное число является палиндромом");
+}
+else 
+    Console.WriteLine("Введенное число не является палиндромом");  
 
 /* int a = 12329;
 int b = new int();
@@ -12,20 +36,3 @@ c = a / 1000;
 Console.WriteLine(b);
 Console.WriteLine(c);
 */
-//Int32.Parse
- 
-            int number, result = 0;
-        
-            Console.Write("enter number: ");
-            number = Convert.ToInt32(Console.ReadLine());
- 
-            while (number > 0) {
-                result *= 10;
-                result += number % 10;
-                number /= 10;
-            }
- 
-            Console.WriteLine(result);
-       
-
-
